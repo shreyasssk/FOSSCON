882c5c61bbc121739fe0c813eeced7227656d40a
shreyasssk <35000429+shreyasssk@users.noreply.github.com>
2019-09-01 22:56:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// PreLoader
+$(window).on('load', function() {  ///to make sure whole page is loaded
+  // $('#box').fadeOut();
+  $('#preloader').delay(350).fadeOut();
+});
+
+// FORM
+// var modal = document.getElementById('modal-wrapper');
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// };
+
+// RADIO BUTTON VALUE
---
+++ table.js
@@ -0,0 +1,9 @@
+// var radioPushed = document.getElementByName('nameOfradio').value;
+// var queryString = "?radioPushed=" + radioPushed;
+// console.log(queryString);
+
+var x = document.querySelector('input[name=nameOfRadio]:checked').value;
+console.log(x);
+
+var x = $('input[name=radioName]:checked').val();
+console.log(x);
